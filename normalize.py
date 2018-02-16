bd69c241c525b0586c328331d5ce347f2ff675bf
Muhammad Owais Bawany <muhammadowaisbawany@gmail.com>
2018-02-16 00:30:10 -0500

    averaging and dividing every value by avergae

--- normalize.py
+++ normalize.py
@@ -14,23 +14,46 @@ path = '/Users/obawany/Downloads/GDC_Downloads/Adrenal Gland/female/0a0a8e16-d3a
 #data.columns = ["a"]
 #values = data.pop('a')
 
-#data = np.loadtxt("/Users/obawany/Desktop/GItHub Repositories/Normalizing-data-using-python/3f42ef68-68d9-428b-be89-48da95336f3e.htseq.counts.txt", usecols=(1,))
+data = np.loadtxt("/Users/obawany/Desktop/GItHub Repositories/Normalizing-data-using-python/3f42ef68-68d9-428b-be89-48da95336f3e.htseq.counts.txt", usecols=(1,))
 #data = data/data[0]
 
-data = np.loadtxt("/Users/obawany/Desktop/GItHub Repositories/Normalizing-data-using-python/test.txt", usecols=(1,))
+data1 = np.loadtxt("/Users/obawany/Desktop/GItHub Repositories/Normalizing-data-using-python/test.txt", usecols=(1,))
 #data.sum(axis=0)
 
 #df = pd.DataFrame((9), reshape(3,3))
 
-Count_row=data.shape[0]
-logging.info(Count_row)
-logging.info(data.index)
+#Count_row=data.shape[0]
+#logging.info(Count_row)
+#logging.info(data.index)
 #write(Count_row)
 #len(df.index)
 
 #len(data.index)
 
-#int n = data.length()
+datasum1 = 0
 
-data = data/data[4]
+for i in range(0, len(data1) ):
+	datasum1 = datasum1 + data1[i]
+
+for i in range(0, len(data1)):
+	data1[i] = data1[i]/datasum1
+
+
+#int n = data.length()
+#data1 = data1/data1[1]
+np.savetxt('testnew.txt', data1)
+
+datasum = 0
+
+
+for i in range(0, len(data) ):
+	datasum = datasum + data[i]
+
+for i in range(0, len(data)):
+	data[i] = data[i]/datasum
+
+
+
+
+#data = data/data[600]
 np.savetxt('new.txt', data)
