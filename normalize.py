69f560c6eb621b2f4933d001a5c728ce9c0428bc
Muhammad Owais Bawany <muhammadowaisbawany@gmail.com>
2018-02-15 22:36:37 -0500

    test file and csv import

--- normalize.py
+++ normalize.py
@@ -1,7 +1,8 @@
 #import numpy as np
 import pandas as pd
+import csv
 
 #data = np.loadtxt('.txt')
-data = pd.read_csv('/Users/obawany/Downloads/GDC_Downloads/Adrenal Gland/female/0a0a8e16-d3af-4e98-802e-761966588e4f/3f42ef68-68d9-428b-be89-48da95336f3e.htseq.counts.txt', sep=" ", header = None)
-data.columns = ["Gene", "count"]
+data = pd.read_csv('3f42ef68-68d9-428b-be89-48da95336f3e.htseq.counts', sep=" ", header = None)
+data.columns = ["a", "b"]
 
